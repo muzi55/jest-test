606a402300fd069d9651ac4c6fccd3da5bd6588a
muzi5@kakao.com <muzi5@kakao.com>
2024-10-15 16:18:55 +0900

    toBe, toStrictEqual, toMatchObject 사용

--- src/toMatchObject/toMatcjObject.spec.ts
+++ src/toMatchObject/toMatcjObject.spec.ts
@@ -1,6 +1,8 @@
 import { obj } from "./toMatchObject";
 
-test("객체를 비교하여라", () => {
+test("객체를 비교하여라(클래스)", () => {
   //   expect(obj('hello')).not.toBe({ a: "hello" });
   expect(obj("hello")).toMatchObject({ a: "hello" });
+  expect(obj("hello")).not.toBe({ a: "hello" });
+  expect(obj("hello")).not.toStrictEqual({ a: "hello" });
 });
--- src/toStricEqual/toStrictEqual.spec.ts
+++ src/toStricEqual/toStrictEqual.spec.ts
@@ -1,8 +1,14 @@
 import { arr, obj } from "./toStrictEqual";
 
-test("객체를 비교하여라(클래스)", () => {
+test("객체를 비교하여라", () => {
   expect(obj()).not.toBe({ a: "hello" });
   expect(obj()).toStrictEqual({ a: "hello" });
   expect(arr()).toStrictEqual([1, 2, 3]);
   // expect(arr().toBe([1, 2, 3]));
+});
+
+test("배열을 비여하여라", () => {
+  expect(arr()).not.toBe([1, 2, 3]);
+  expect(arr()).toStrictEqual([1, 2, 3]);
+  // expect([1, 2, 3]).toBe([1, 2, 3]);
 });
